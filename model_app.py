40e7c44c8384dc20c727b6a10e0c5ba61c4cb34e
Yana B <yana.bahramhan@yandex.ru>
2023-09-23 14:42:53 +0300

    updated model_app.py

--- model_app.py
+++ model_app.py
@@ -5,29 +5,30 @@ import plotly.express as px
 from model_file import url, open_table, split_table, scale_data, load_model_and_predict
 
 st.set_page_config(
+    page_icon = Image.open("images/pills.png"),
     layout="wide",
 )
 
 def show_title():
-    image_up = Image.open("data/myriam-zilles-KltoLK6Mk-g-unsplash(up).jpg")
+    image_up = Image.open("images/myriam-zilles-KltoLK6Mk-g-unsplash(up).jpg")
     st.image(image_up)
-    st.title("Предсказание стоимости страховки на здоровье")
-    st.header("на основе модели линейной регрессии")
+    st.title("Prediction insurance cost")
+    st.header("based on Linear regression model")
     st.divider()
 
 def main_page():
     col1, col2 = st.columns([0.3, 0.7], gap = "large")
     with col1:
-        st.header("Введите данные")
-        age = st.slider("Возраст", min_value=18, max_value=90, value=30,
+        st.header("Input data")
+        age = st.slider("Age", min_value=18, max_value=90, value=30,
                           step=1)
-        sex = st.radio("Пол", ("Мужской", "Женский"))
-        bmi = st.number_input("Индекс массы тела:", value = 25)
-        children = st.slider("Количество детей", min_value=0, max_value=5, value=1,
+        sex = st.radio("Gender", ("Male", "Female"))
+        bmi = st.number_input("Body mass index:", value = 25)
+        children = st.slider("Children", min_value=0, max_value=5, value=1,
                                step=1)
-        smoker = st.radio("Являетесь ли вы курильщиком?", ("Да", "Нет"))
-        region = st.selectbox("Регион проживания", ("northwest", "southeast", "northeast", "southwest"))
-        translate = {"Мужской": "male", "Женский": "female", "Да": "yes", "Нет": "no"}
+        smoker = st.radio("Smoker", ("Yes", "No"))
+        region = st.selectbox("Region", ("northwest", "southeast", "northeast", "southwest"))
+        translate = {"Male": "male", "Female": "female", "Yes": "yes", "No": "no"}
 
         data = {
             "age": age,
@@ -39,43 +40,41 @@ def main_page():
         }
 
         user_data = pd.DataFrame(data, index=[0])
-        st.subheader("Ваши данные")
+        st.subheader("Your data")
         st.write(user_data)
 
         train_df = open_table(url)
         train_X_df, train_y_df = split_table(train_df)
         full_X_df = pd.concat((user_data, train_X_df), axis=0)
         preprocessed_X_df = scale_data(full_X_df, [], test=False)
-        st.subheader("Предсказание")
+        st.subheader("Prediction")
         prediction = load_model_and_predict(preprocessed_X_df)
         if prediction > 0:
-            st.info(f"Предсказанная стоимость страховки: {prediction}")
+            st.markdown(f"Predicted insurance cost: **{prediction}**")
         else:
-            st.info(f"Предсказанная стоимость страховки: {prediction}. Вам страховка не нужна:)")
-
-
+            st.info(f"Predicted insurance cost: **{prediction}**. You don't need an insurance:)")
 
     with col2:
-        st.header("Что влияет на стоимость страховки больше всего?")
-        tab1, tab2, tab3 = st.tabs(["Курение", "Индекс массы тела", "Возраст"])
+        st.header("What cause such insurance cost?")
+        tab1, tab2, tab3 = st.tabs(["Smoking", "Body mass index", "Age"])
 
         with tab1:
-            fig1 = px.box(data_frame = train_df, x = "smoker", y = "charges", points="all")
+            fig1 = px.strip(data_frame = train_df, x = "smoker", y = "charges")
             st.plotly_chart(fig1)
-            st.markdown("Курение оказывает значительное влияние на стоимость страховки. Наличие факта курения повышает страховку на более чем **23000** условные единицы. Поэтому если вы хотите сократить стоимость страховки на здоровье, вам нужно бросить курение в первую очередь.")
+            st.markdown("Smoking has a significant impact on the cost of insurance. The fact of smoking increases insurance by more than **23,000** units. So if you want to reduce the cost of your health insurance, you need to quit smoking first.")
         with tab2:
-            fig2 = px.scatter(data_frame=train_df, x = "bmi", y = "charges",  color = "region", color_discrete_sequence= px.colors.qualitative.Bold)
+            fig2 = px.scatter(data_frame=train_df, x = "bmi", y = "charges", color = "crimson", trendline = "ols")
             st.plotly_chart(fig2)
-            st.write("При увеличении индекса массы тела стоимость страховки на здоровье повышается. После отказа от курения вам нужно сократить значение индекса - похудеть.")
+            st.write("As your body mass index increases, the cost of health insurance increases too. After quitting smoking, you need to reduce the index value - lose weight.")
         with tab3:
-            fig3 = px.scatter(data_frame= train_df, x = "age", y = "charges", color = "bmi", color_continuous_scale= px.colors.sequential.Electric_r)
+            fig3 = px.scatter(data_frame= train_df, x = "age", y = "charges", trendline = "ols")
             st.plotly_chart(fig3)
-            st.write("В больших возрастах стоимость страховки увеличивается - это необходимо учитывать при покупке страховки на здоровье.")
+            st.write("At older ages, the cost of insurance increases - this must be taken into account when purchasing health insurance.")
 
 def additional_info():
     st.divider()
-    st.write("Использованный набор данных был взят из репозитория по ссылке: https://github.com/stedy/Machine-Learning-with-R-datasets/blob/master/insurance.csv")
-    image_down = Image.open("data/myriam-zilles-KltoLK6Mk-g-unsplash(down).jpg")
+    st.write("The data was retrived from the repository via the [link](https://github.com/stedy/Machine-Learning-with-R-datasets/blob/master/insurance.csv).")
+    image_down = Image.open("images/myriam-zilles-KltoLK6Mk-g-unsplash(down).jpg")
     st.image(image_down)
 
 def process_main_page():
@@ -85,5 +84,3 @@ def process_main_page():
 
 if __name__ == "__main__":
     process_main_page()
-
-
